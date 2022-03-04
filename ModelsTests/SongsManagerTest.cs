aba97229b97c737ec8a234a18d6dcdc7c5421bf3
Aymeric Meindre <aym.meindre@gmail.com>
2022-03-04 15:51:39 +0100

    Add test for update with wrong credentials

--- ModelsTests/SongsManagerTest.cs
+++ ModelsTests/SongsManagerTest.cs
@@ -148,9 +148,52 @@ namespace ModelsTests.SongsManagerTest
             Assert.False(File.Exists(expectedSong.LocalPath + "audio.wav"));
 
             string errorMessage = await songsManager.updateSongAsync(expectedSong);
+
             Assert.True(File.Exists(version.VersionPath + expectedSong.LocalPath + "audio.wav"));
             Assert.True(File.Exists(expectedSong.LocalPath + "audio.wav"));
             Assert.Equal(string.Empty, errorMessage);
+        }
+
+        [Fact]
+        public async Task TryUpdateSongWithWrongGitLabUsernameTest()
+        {
+            //Add song for synchronization
+            songsManager.addSong(title, file, localPath);
+            //Simulate a change on version workspace
+            Directory.CreateDirectory(version.VersionPath + expectedSong.LocalPath);
+            FileStream fileStream = File.Create(version.VersionPath + expectedSong.LocalPath + "audio.wav");
+            fileStream.Close();
+            Assert.True(File.Exists(version.VersionPath + expectedSong.LocalPath + "audio.wav"));
+            Assert.False(File.Exists(expectedSong.LocalPath + "audio.wav"));
+
+            user.GitLabUsername = "Wrong Username";
+
+            string errorMessage = await songsManager.updateSongAsync(expectedSong);
+
+            Assert.True(File.Exists(version.VersionPath + expectedSong.LocalPath + "audio.wav"));
+            Assert.False(File.Exists(expectedSong.LocalPath + "audio.wav"));
+            Assert.Equal("Error Bad Credentials", errorMessage);
+        }
+
+        [Fact]
+        public async Task TryUpdateSongWithWrongGitLabPasswordTest()
+        {
+            //Add song for synchronization
+            songsManager.addSong(title, file, localPath);
+            //Simulate a change on version workspace
+            Directory.CreateDirectory(version.VersionPath + expectedSong.LocalPath);
+            FileStream fileStream = File.Create(version.VersionPath + expectedSong.LocalPath + "audio.wav");
+            fileStream.Close();
+            Assert.True(File.Exists(version.VersionPath + expectedSong.LocalPath + "audio.wav"));
+            Assert.False(File.Exists(expectedSong.LocalPath + "audio.wav"));
+
+            user.GitLabPassword = "Wrong Password";
+
+            string errorMessage = await songsManager.updateSongAsync(expectedSong);
+
+            Assert.True(File.Exists(version.VersionPath + expectedSong.LocalPath + "audio.wav"));
+            Assert.False(File.Exists(expectedSong.LocalPath + "audio.wav"));
+            Assert.Equal("Error Bad Credentials", errorMessage);
         }
 
     }
