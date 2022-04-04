64f52e8e00a863e414c910f52e6e90fc17e8d4ba
Aymeric Meindre <aym.meindre@gmail.com>
2022-04-04 14:41:28 +0200

    Clean up code

--- ViewModels/UserViewModel.cs
+++ ViewModels/UserViewModel.cs
@@ -4,14 +4,6 @@ namespace App1.ViewModels
 {
     public class UserViewModel : Bindable
     {
-        public UserViewModel()
-        {
-            this.bandName_ = string.Empty;
-            this.bandPassword_ = string.Empty;
-            this.username_ = string.Empty;
-            this.bandEmail_ = string.Empty;
-        }
-
         public UserViewModel(User user)
         {
             this.bandName_ = user.BandName;
@@ -20,23 +12,15 @@ namespace App1.ViewModels
             this.bandEmail_ = user.BandEmail;
         }
 
-        public UserViewModel(string BandName, string BandPassword, string Username, string BandEmail)
-        {
-            this.bandName_ = BandName;
-            this.bandPassword_ = BandPassword;
-            this.username_ = Username;
-            this.bandEmail_ = BandEmail;
-        }
-
         public override bool Equals(object? obj)
         {
-            var song = obj as UserViewModel;
-            if (song == null)
+            var user = obj as UserViewModel;
+            if (user == null)
                 return false;
-            if (this.BandName != song.BandName ||
-               this.BandPassword != song.BandPassword ||
-               this.Username != song.Username ||
-               this.BandEmail != song.BandEmail)
+            if (this.BandName != user.BandName ||
+               this.BandPassword != user.BandPassword ||
+               this.Username != user.Username ||
+               this.BandEmail != user.BandEmail)
             {
                 return false;
             }
@@ -55,10 +39,6 @@ namespace App1.ViewModels
             {
                 return bandName_;
             }
-            set
-            {
-                SetProperty(ref bandName_, value);
-            }
         }
         private string bandEmail_;
         public string BandEmail
@@ -66,10 +46,6 @@ namespace App1.ViewModels
             get
             {
                 return bandEmail_;
-            }
-            set
-            {
-                SetProperty(ref bandEmail_, value);
             }
         }
         private string bandPassword_;
@@ -79,10 +55,6 @@ namespace App1.ViewModels
             {
                 return bandPassword_;
             }
-            set
-            {
-                SetProperty(ref bandPassword_, value);
-            }
         }
         private string username_;
         public string Username
@@ -90,10 +62,6 @@ namespace App1.ViewModels
             get
             {
                 return username_;
-            }
-            set
-            {
-                SetProperty(ref username_, value);
             }
         }
     }
