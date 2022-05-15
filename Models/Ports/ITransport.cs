b51ef94af57de7166ef106a7d0a2ba7062f84fd3
Aymeric Meindre <aym.meindre@gmail.com>
2022-05-15 23:44:28 +0200

    Fix Sonar Issue

--- Models/Ports/ITransport.cs
+++ Models/Ports/ITransport.cs
@@ -2,15 +2,15 @@
 {
     public interface ITransport
     {
-        void init(string path, string name);
-        Task initAsync(string musicSyncSongPath, string sharedLink);
+        void init(string songMusicSyncPath, string name);
+        Task initAsync(string songMusicSyncPath, string sharedLink);
         Task uploadAllFilesAsync(string songMusicSyncPath, string title, string description);
         Task uploadFileAsync(string songMusicSyncPath, string file, string title);
         void tag(string songMusicSyncPath, string versionNumber);
         Task<bool> updatesAvailbleAsync(string songMusicSyncPath);
         Task downloadLastUpdateAsync(string songMusicSyncPath);
         Task revertToLastLocalVersionAsync(string songMusicSyncPath);
-        Task<SongVersion>? lastLocalVersionAsync(string musicSyncSongPath);
+        Task<SongVersion>? lastLocalVersionAsync(string songMusicSyncPath);
         bool initiated(string songMusicSyncPath);
         Task<List<SongVersion>>? localVersionsAsync(string songMusicSyncPath);
         Task<List<SongVersion>>? upcomingVersionsAsync(string songMusicSyncPath);
