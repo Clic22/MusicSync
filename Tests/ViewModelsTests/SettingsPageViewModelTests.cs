7f5727e1c48584b016de865a96f57b4da9424a94
Aymeric Meindre <aym.meindre@gmail.com>
2022-05-22 11:34:18 +0200

    Add test to check binding settings

--- Tests/ViewModelsTests/SettingsPageViewModelTests.cs
+++ Tests/ViewModelsTests/SettingsPageViewModelTests.cs
@@ -15,7 +15,7 @@ namespace ViewModelsTests.SettingsPageViewModelTests
     {
 
         [Fact]
-        public void initializedSettingsTest()
+        public void InitializedSettingsTest()
         {
             //Setup
             string BandName = "BandName";
@@ -23,11 +23,11 @@ namespace ViewModelsTests.SettingsPageViewModelTests
             string Username = "Username";
             string BandEmail = "BandEmail@gmail.com";
             string expectedMusicSyncFolder = @"./SongsManagerTest/End of the Road/";
-            User user = new User(BandName, BandPassword, Username, BandEmail);
-            UserViewModel expectedUserViewModel = new UserViewModel(user);
-            Mock<ISaver> saverMock = new Mock<ISaver>();
+            var expectedUser = new User(BandName, BandPassword, Username, BandEmail);
+            var expectedUserViewModel = new UserViewModel(expectedUser);
+            var saverMock = new Mock<ISaver>();
             saverMock.Setup(m => m.SavedMusicSyncFolder()).Returns(expectedMusicSyncFolder);
-            saverMock.Setup(m => m.SavedUser()).Returns(user);
+            saverMock.Setup(m => m.SavedUser()).Returns(expectedUser);
 
             IFileManager fileManager = new FileManager();
             SettingsPageViewModel viewModel = new SettingsPageViewModel(saverMock.Object);
@@ -36,10 +36,42 @@ namespace ViewModelsTests.SettingsPageViewModelTests
             saverMock.Verify(m => m.SavedUser(), Times.Once());
             Assert.Equal(expectedUserViewModel, viewModel.Settings.User);
             Assert.Equal(expectedMusicSyncFolder, viewModel.Settings.MusicSyncFolder);
+            
         }
 
         [Fact]
-        public void saveSettingsTest()
+        public void ChangeSettingsTest()
+        {
+            //Setup
+            string BandName = "BandName";
+            string BandPassword = "BandPassword";
+            string Username = "Username";
+            string BandEmail = "BandEmail@gmail.com";
+            string expectedMusicSyncFolder = @"./SongsManagerTest/End of the Road/";
+            var expectedUser = new User(BandName, BandPassword, Username, BandEmail);
+            var expectedUserViewModel = new UserViewModel(expectedUser);
+            var saverMock = new Mock<ISaver>();
+            saverMock.Setup(m => m.SavedMusicSyncFolder()).Returns(expectedMusicSyncFolder);
+            saverMock.Setup(m => m.SavedUser()).Returns(expectedUser);
+
+            IFileManager fileManager = new FileManager();
+            SettingsPageViewModel viewModel = new SettingsPageViewModel(saverMock.Object);
+
+            Action action = () => viewModel.Settings.User.Username = "New Username";
+            Assert.PropertyChanged(viewModel.Settings.User, "Username", action);
+            action = () => viewModel.Settings.User.BandName = "New BandName";
+            Assert.PropertyChanged(viewModel.Settings.User, "BandName", action);
+            action = () => viewModel.Settings.User.BandPassword = "New BandPassword";
+            Assert.PropertyChanged(viewModel.Settings.User, "BandPassword", action);
+            action = () => viewModel.Settings.User.BandEmail = "New BandEmail";
+            Assert.PropertyChanged(viewModel.Settings.User, "BandEmail", action);
+
+            action = () => viewModel.Settings.MusicSyncFolder = "New MusicSyncFolder Location";
+            Assert.PropertyChanged(viewModel.Settings, "MusicSyncFolder", action);
+        }
+
+        [Fact]
+        public void SaveSettingsTest()
         {
             //Setup
             string BandName = "BandName";
